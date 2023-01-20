ac45ecb46bfe52d6eb7541554e6e0deb074092e8
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-19 18:10:05 -0600

    created attack functions, added jQuery to render function and working on the dead animation

--- script.js
+++ script.js
@@ -5,14 +5,100 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+let fungusHP = 100;
+let myAP = 100;
+let mushieAnimation = 'walk';
+
 function onReady() {
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
-    
+    $('.attack-btn.arcane-sceptre').on('click', arcaneAttack);
+    $('.attack-btn.entangle').on('click', entangleAttack);
+    $('.attack-btn.dragon-blade').on('click', dragonBladeAttack);
+    $('.attack-btn.star-fire').on('click', starFireAttack);
     
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+}
+
+function arcaneAttack() {
+    fungusHP -= 14;
+    myAP -= 12;
+    if (fungusHP < 0){
+        fungusHP = 0;
+    }
+
+    if (myAP < 0){
+        myAP = 0;
+    }
+    console.log('this is arcane attack', fungusHP, myAP);
+
+    render();
+}
+
+function entangleAttack() {
+    fungusHP -= 9;
+    myAP -= 23;
+    if (fungusHP < 0){
+        fungusHP = 0;
+    }
+
+    if (myAP < 0){
+        myAP = 0;
+    }
+    console.log('this is entangle attack', fungusHP, myAP);
+
+    render();
+}
+
+function dragonBladeAttack() {
+    fungusHP -= 47;
+    myAP -= 38;
+    if (fungusHP < 0){
+        fungusHP = 0;
+    }
+
+    if (myAP < 0){
+        myAP = 0;
+    }
+    console.log('this is dragon blade attack', fungusHP, myAP);
+
+    render();
+}
+
+function starFireAttack() {
+    fungusHP -= 25;
+    myAP -= 33;
+    if (fungusHP < 0){
+        fungusHP = 0;
+    }
+
+    if (myAP < 0){
+        myAP = 0;
+    }
+    console.log('this is star fire attack', fungusHP, myAP);
+
+    render();
+}
+
+function render() {
+    $('.hp-text').text(fungusHP + ' HP');
+    $('.ap-text').text(myAP + ' AP');
+
+    $('#hp-meter').val(fungusHP);
+    $('#ap-meter').val(myAP);
+
+    if (fungusHP <= 0){
+        let mushieAnimation = 'dead';
+        $('.freaky-fungus walk').attr('class', mushieAnimation);
+    }
+    console.log(mushieAnimation);
+
+
+
+    
+
 }
